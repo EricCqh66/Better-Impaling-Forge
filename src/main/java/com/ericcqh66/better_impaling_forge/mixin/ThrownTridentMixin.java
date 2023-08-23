0c21027fec706df4575f39c562c167b1307e5742
Eric <qhchang2019@163.com>
2023-08-23 13:47:19 +0800

    WIN!!!

---
+++ src/main/java/com/ericcqh66/better_impaling_forge/mixin/ThrownTridentMixin.java
@@ -0,0 +1,20 @@
+package com.ericcqh66.better_impaling_forge.mixin;
+
+import net.minecraft.world.entity.projectile.ThrownTrident;
+import net.minecraft.world.phys.EntityHitResult;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import com.ericcqh66.better_impaling_forge.BetterImpalingForge;
+
+@Mixin(ThrownTrident.class)
+public abstract class ThrownTridentMixin{
+    @Inject(
+        method = "onHitEntity",
+        at = @At(value = "HEAD")
+    )
+    private void onHitEntityMixin(EntityHitResult p_37573_, CallbackInfo ci) {
+        BetterImpalingForge.is_in_water_or_rain = p_37573_.getEntity().isInWaterOrRain();
+    }
+}
